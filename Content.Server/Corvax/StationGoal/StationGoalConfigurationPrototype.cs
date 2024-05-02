e6acbff07ab1f2b0c2b3e299172a91ceea0ed334
Pupchansky <puppochta@gmail.com>
2024-05-02 08:15:10 +0500

    Added a multi-goal system

---
+++ Content.Server/Corvax/StationGoal/StationGoalConfigurationPrototype.cs
@@ -0,0 +1,19 @@
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.Corvax.StationGoal
+{
+    [DataDefinition]
+    [Serializable, Prototype("stationGoalConfiguration")]
+    public sealed partial class StationGoalConfigurationPrototype : IPrototype
+    {
+        [ViewVariables]
+        [IdDataField]
+        public string ID { get; private set; } = default!;
+
+        [DataField] public int MinGoals { get; private set; } = 1;
+
+        [DataField] public int MaxGoals { get; private set; } = 4;
+
+        [DataField] public int Priority { get; private set; } = 0;
+    }
+}
