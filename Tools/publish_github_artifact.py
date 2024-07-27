86d7dfce53d59e76d147cdcc28034dd6c27f62b6
YesImMorty <164863999+YesImMorty@users.noreply.github.com>
2024-07-27 15:15:57 +0300

    Update publish_github_artifact.py

--- Tools/publish_github_artifact.py
+++ Tools/publish_github_artifact.py
@@ -38,7 +38,8 @@ def main():
 def get_artifact_url() -> str:
     headers = {
         "Authorization": f"Bearer {GITHUB_TOKEN}",
-        "X-GitHub-Api-Version": "2022-11-28"
+        "X-GitHub-Api-Version": "2022-11-28",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
     resp = requests.get(f"https://api.github.com/repos/{GITHUB_REPOSITORY}/actions/artifacts/{ARTIFACT_ID}/zip", allow_redirects=False, headers=headers)
     resp.raise_for_status()
