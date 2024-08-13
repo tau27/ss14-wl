d727cc31fcbf655963539a542769d98796d322ae
ArRei <arka4el@outlook.com>
2024-08-13 17:45:48 +0300

    "персона среднего возраста" теперь отображается не в 30, а в 35 лет минимум

--- Content.Shared/Humanoid/Prototypes/SpeciesPrototype.cs
+++ Content.Shared/Humanoid/Prototypes/SpeciesPrototype.cs
@@ -127,7 +127,7 @@ public sealed partial class SpeciesPrototype : IPrototype
     ///     Characters younger than this appear young.
     /// </summary>
     [DataField]
-    public int YoungAge = 30;
+    public int YoungAge = 35;
 
     /// <summary>
     ///     Characters older than this appear old. Characters in between young and old age appear middle aged.
