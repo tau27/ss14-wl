0880c0234866ad5266907822a8901ad0dfca2364
mosleyos <124694589+mosleyos@users.noreply.github.com>
2024-10-06 18:48:07 +0300

    update

--- Content.Shared/_WL/CCVars/WLCCVars.cs
+++ Content.Shared/_WL/CCVars/WLCCVars.cs
@@ -24,18 +24,6 @@ public sealed class WLCVars
         CVarDef.Create("game.is_age_check_needed", true, CVar.REPLICATED);
 
     /*
-     *  HTTP API
-     */
-    /// <summary>
-    /// Токен для авторизации htpp-запросов на api сервера.
-    /// </summary>
-    public static readonly CVarDef<string> WLApiToken =
-        CVarDef.Create(
-            "admin.wl_api_token", "92132c05009d46c25ffa1d7263b8f24226abef8a7503ce7c26175b0f0e3db61dc82907a3bd7b72f8321206fc42576bb2896c9a937714d2cbf422d3507fc078492cedd3fa6300eb8fa75f4ceffe8577c6790bc0a93ea989e9cbc15e090dff97eb",
-            CVar.SERVERONLY | CVar.CONFIDENTIAL,
-            "Строковой токен, использующийся для авторизации HTTP-запросов, отправленных на API сервера.");
-
-    /*
      *  Discord
      */
     /// <summary>
