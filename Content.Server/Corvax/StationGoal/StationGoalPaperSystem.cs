0597aa84469955e486dbff633f816bb803e3ef4c
mosleyos <124694589+mosleyos@users.noreply.github.com>
2024-11-28 19:24:30 +0300

    Update StationGoalPaperSystem.cs

--- Content.Server/Corvax/StationGoal/StationGoalPaperSystem.cs
+++ Content.Server/Corvax/StationGoal/StationGoalPaperSystem.cs
@@ -190,7 +190,7 @@ namespace Content.Server.Corvax.StationGoal
 
         public string FormatStringToGoalContent(string content, string station)
         {
-            var dateString = DateTime.Now.AddYears(1000).ToString("dd.MM.yyyy");
+            var dateString = DateTime.Now.AddYears(172).ToString("dd.MM.yyyy");
 
             var toReplace = new Dictionary<string, string>();
 
