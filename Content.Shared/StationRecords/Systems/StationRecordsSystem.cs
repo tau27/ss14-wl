3d19842c9423d67ca47a106dad5e63ea17e9065f
Pupchansky <139097101+Pupchansky@users.noreply.github.com>
2026-07-24 17:43:41 +0500

    Upstream fixes 1 (#756)
    * послеапстримские фиксы записей
    
    * фикс проблем с манифестом и новостями

--- Content.Shared/StationRecords/Systems/StationRecordsSystem.cs
+++ Content.Shared/StationRecords/Systems/StationRecordsSystem.cs
@@ -1,3 +1,5 @@
+using Content.Shared._WL.Languages;
+using Content.Shared._WL.Languages.Components;
 using Content.Shared.Access.Components;
 using Content.Shared.Access.Systems;
 using Content.Shared.Forensics.Components;
@@ -9,7 +11,9 @@ using Content.Shared.Roles;
 using Content.Shared.StationRecords.Components;
 using Content.Shared.StationRecords.Events;
 using Robust.Shared.Enums;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
+using System.Linq;
 
 namespace Content.Shared.StationRecords.Systems;
 
@@ -38,6 +42,10 @@ public sealed partial class StationRecordsSystem : EntitySystem
     [Dependency] private InventorySystem _inventory = default!;
     [Dependency] private StationRecordKeyStorageSystem _keyStorage = default!;
     [Dependency] private SharedIdCardSystem _idCard = default!;
+
+    // WL-Changes-start
+    [Dependency] private SharedRoleSystem _role = default!;
+    // WL-Changes-end
 
     [Dependency] private EntityQuery<IdCardComponent> _idCardQuery = default!;
     [Dependency] private EntityQuery<PdaComponent> _pdaQuery = default!;
@@ -97,6 +105,10 @@ public sealed partial class StationRecordsSystem : EntitySystem
         _fingerprintQuery.TryComp(player, out var fingerprintComponent);
         _dnaQuery.TryComp(player, out var dnaComponent);
 
+        // WL-Changes-start
+        TryComp<LanguagesComponent>(player, out var languageComponent);
+        // WL-Changes-end
+
         CreateGeneralRecord(
             station,
             idUid.Value,
@@ -107,7 +119,8 @@ public sealed partial class StationRecordsSystem : EntitySystem
             jobId,
             fingerprintComponent?.Fingerprint,
             dnaComponent?.DNA,
-            profile);
+            profile,
+            /*WL-Changes-start*/languageComponent?.Speaking.ToList() ?? []/*WL-Changes-end*/);
     }
 
     /// <summary>
@@ -146,14 +159,15 @@ public sealed partial class StationRecordsSystem : EntitySystem
         string jobId,
         string? mobFingerprint,
         string? dna,
-        HumanoidCharacterProfile profile)
+        HumanoidCharacterProfile profile,
+        /*WL-Changes-start*/List<ProtoId<LanguagePrototype>> languages/*WL-Changes-end*/)
     {
         if (!ProtoMan.TryIndex<JobPrototype>(jobId, out var jobPrototype))
             throw new ArgumentException($"Invalid job prototype ID: {jobId}");
 
         // when adding a record that already exists use the old one
         // this happens when respawning as the same character
-        if (GetRecordByName(station.AsNullable(), name) is {} id)
+        if (GetRecordByName(station.AsNullable(), name) is { } id)
         {
             SetIdKey(idUid, new StationRecordKey(id, station));
             return;
@@ -163,14 +177,27 @@ public sealed partial class StationRecordsSystem : EntitySystem
         {
             Name = name,
             Age = age,
-            JobTitle = jobPrototype.LocalizedName,
+            // WL-Changes-start
+            JobTitle = _role.GetSubname(profile, jobId) ?? jobPrototype.LocalizedName,
+            // WL-Changes-end
             JobIcon = jobPrototype.Icon,
             JobPrototype = jobId,
             Species = species,
             Gender = gender,
             DisplayPriority = jobPrototype.RealDisplayWeight,
             Fingerprint = mobFingerprint,
-            DNA = dna
+            DNA = dna,
+            // WL-Changes-start
+            EmploymentRecord = profile.EmploymentRecord,
+            MedicalRecord = profile.MedicalRecord,
+            SecurityRecord = profile.SecurityRecord,
+            Confederation = profile.Confederation,
+            Country = profile.Country,
+            DateOfBirth = profile.DateOfBirth,
+            Height = profile.Height,
+            Fullname = profile.FullName,
+            Languages = languages
+            // WL-Changes-end
         };
 
         var key = AddRecordEntry(station.AsNullable(), record);
